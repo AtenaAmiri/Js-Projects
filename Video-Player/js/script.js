3b0511a764bd4d01e88df2e4cdfea82572034746
atena <ftemeh.amiirii@gmail.com>
2023-02-17 12:34:29 +0330

    video player

--- Video-Player/js/script.js
+++ Video-Player/js/script.js
@@ -1,3 +1,4 @@
+const player = document.querySelector(".player");
 const video = document.querySelector("video");
 const progressRange = document.querySelector(".progress-range");
 const progressBar = document.querySelector(".progress-bar");
@@ -5,7 +6,7 @@ const playBtn = document.getElementById("play-btn");
 const volumeIcon = document.getElementById("volume-icon");
 const volumeRange = document.querySelector(".volume-range");
 const volumeBar = document.querySelector(".volume-bar");
-const speed = document.querySelector(".player-speed");
+const speed = document.querySelector("#player-speed");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
@@ -28,6 +29,7 @@ function togglePlay() {
 }
 // On video end,show play button icon
 video.addEventListener("ended", showPlayIcon);
+
 // Progress Bar ---------------------------------- //
 
 //Calculate display time format
@@ -54,6 +56,9 @@ function setProgress(e) {
 }
 
 // Volume Controls --------------------------- //
+
+let lastVolume = 1;
+
 //Volume Bar
 function changeVolume(e) {
   let volume = e.offsetX / volumeRange.offsetWidth;
@@ -64,12 +69,81 @@ function changeVolume(e) {
   if (volume > 0.9) {
     volume = 1;
   }
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+  //Change Icon depending on volume
+  volumeIcon.className = "";
+  if (volume > 0.7) {
+    volumeIcon.classList.add("fas", "fa-volume-up");
+  } else if (volume < 0.7 && volume > 0) {
+    volumeIcon.classList.add("fas", "fa-volume-down");
+  } else if (volume === 0) {
+    volumeIcon.classList.add("fas", "fa-volume-off");
+  }
+  lastVolume = volume;
+}
+
+// Mute  / UnMute
+function toggleMute() {
+  volumeIcon.className = "";
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add("fas", "fa-volume-mute");
+    volumeIcon.setAttribute("title", "Unmute");
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add("fas", "fa-volume-up");
+    volumeIcon.setAttribute("title", "Mute");
+  }
 }
 
 // Change Playback Speed -------------------- //
+function changeSpeed() {
+  video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
+/* View in fullscreen */
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+}
+
+let fullScreen = false;
+
+//Toggle Fullscreen
+function toggleFullscreen() {
+  if (!fullScreen) {
+    openFullscreen(player);
+  } else {
+    closeFullscreen();
+  }
+  fullScreen = !fullScreen;
+}
 //Event Listener
 playBtn.addEventListener("click", togglePlay);
 video.addEventListener("click", togglePlay);
@@ -77,3 +151,6 @@ video.addEventListener("timeupdate", updateProgress);
 video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
+volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
+fullscreenBtn.addEventListener("click", toggleFullscreen);
