0b86653ccadca0bd29989665ba65de5ca440f122
atena <ftemeh.amiirii@gmail.com>
2023-02-23 21:07:18 +0330

    Expanding-Cards

--- Expanding-Cards/js/script.js
+++ Expanding-Cards/js/script.js
@@ -1,6 +1,14 @@
 const panels = document.querySelectorAll(".panel");
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
+    removeActiveClasses();
     panel.classList.add("active");
+    panel.getElementsByClassName.borderRadius = "50px";
   });
 });
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
