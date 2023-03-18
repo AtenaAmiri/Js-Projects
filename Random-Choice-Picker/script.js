284ca0a89eb1bd047ce379820ceb05e1e7d1c927
atena <ftemeh.amiirii@gmail.com>
2023-03-18 14:50:52 +0330

    Random-Choice-Picker

--- Random-Choice-Picker/script.js
+++ Random-Choice-Picker/script.js
@@ -8,7 +8,7 @@ textarea.addEventListener("keyup", (e) => {
 
   if (e.key === "Enter") {
     setTimeout(() => {
-      e.target.value = "HI";
+      e.target.value = "";
     }, 10);
     randomSelect();
   }
@@ -36,17 +36,29 @@ function randomSelect() {
   const interval = setInterval(() => {
     const randomTag = pickRandomTag();
 
-    highlightTag(randomTag);
+    if (randomTag !== undefined) {
+      highlightTag(randomTag);
+
+      setTimeout(() => {
+        unHighlightTag(randomTag);
+      }, 100);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
 
     setTimeout(() => {
-      unHighlightTag(randomTag);
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
     }, 100);
-  }, 100);
+  }, times * 100);
 }
 
 function pickRandomTag() {
   const tags = document.querySelectorAll(".tag");
-  return tags[Math.floor(Math.random * tags.length)];
+  return tags[Math.floor(Math.random() * tags.length)];
 }
 
 function highlightTag(tag) {
